fed5dc33bba0174c0fc5b70ee671f89488182e91
Victor <victorv.pacheco@yahoo.com.br>
2026-03-19 19:09:36 -0300

    paginacao

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -44,6 +44,38 @@ const animate = keyframes`
     }
 `;
 
+export const List = styled.ul`
+    list-style: none;
+    margin-top: 20px;
+
+    li{
+        padding: 15px 0;
+        display: flex;
+        flex-direction:row;
+        align-items: center;
+        justify-content: space-between;
+
+        & + li{
+            border-top:1px solid purple;
+        }
+
+        a{
+            color: #0d2636;
+            text-decoration: none;
+        }
+    }
+    `;
+
+    export const DeleteButton = styled.button.attrs({
+        type:'button'
+    })`
+        margin-left:6px;
+        background: transparent;
+        color: #0d2636;
+        border: 0;
+        padding: 8px 7px;
+        border-radius: 4px;
+    `;
 
 export const SubmitButton = styled.button.attrs(props => ({
     type:'submit',
@@ -69,39 +101,9 @@ export const SubmitButton = styled.button.attrs(props => ({
             animation: ${animate} 2s linear infinite;
         }
     `
-    }
-`;
+    }`;
 
-export const List = styled.ul`
-list-style: none;
-margin-top: 20px;
 
-li{
-    padding: 15px 0;
-    display: flex;
-    flex-direction:row;
-    align-items: center;
-    justify-content: space-between;
 
-    & + li{
-        border-top:1px solid purple;
-    }
-
-    a{
-        color: #0d2636;
-        text-decoration: none;
-    }
-}
-`;
-
-export const DeleteButton = styled.button.attrs({
-    type:'button'
-})`
-    margin-left:6px;
-    background: transparent;
-    color: #0d2636;
-    border: 0;
-    padding: 8px 7px;
-    border-radius: 4px;
-`;
+    
 
